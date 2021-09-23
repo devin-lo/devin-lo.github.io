230e3b500f4032d641fa4c9abf72434340f048eb
devin <75116968+devin-lo@users.noreply.github.com>
2021-09-23 16:51:40 -0400

    Update riscv_util.js
    prevent anything being done if outside of bounds

--- riscv_util.js
+++ riscv_util.js
@@ -1,21 +1,24 @@
 function luiCalc() {
     var a = document.getElementById("inputNum").value;
     var probNum = parseInt(a);
+	
+	if (a >= -2147483648 && a <= 2147481599) {
+		var rem = probNum % 4096;
+		var luiMod = 0;
+		if (rem > 2047 && rem < 4096) {
+			luiMod++;
+		}
+		else if (rem < -2048 && rem > -4096) {
+			luiMod--;
+		}
+		var luiAmt = Math.floor(probNum / 4096) + luiMod;
+		var addiAmt = rem;
+		if (addiAmt < -2048) {
+			addiAmt += 4096;
+		}
 
-    var rem = probNum % 4096;
-    var luiMod = 0;
-    if (rem > 2047 && rem < 4096) {
-        luiMod++;
-    }
-    else if (rem < -2048 && rem > -4096) {
-        luiMod--;
-    }
-    var luiAmt = Math.floor(probNum / 4096) + luiMod;
-    var addiAmt = rem;
-    if (addiAmt < -2048) {
-        addiAmt += 4096;
-    }
+		document.getElementById("lui").innerHTML = luiAmt;
+		document.getElementById("addi").innerHTML = addiAmt;
+	}
 
-    document.getElementById("lui").innerHTML = luiAmt;
-    document.getElementById("addi").innerHTML = addiAmt;
 }
